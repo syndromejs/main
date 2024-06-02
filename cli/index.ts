b3feb6b963b37ffc3a2460a27f4ad7f940f0a5dc
Jinwoo Jeong <thinkratomos@gmail.com>
2024-06-02 22:02:20 +0900

    feat: change it to clipanion

--- cli/index.ts
+++ cli/index.ts
@@ -1,3 +1,18 @@
-export const echo = (message: string): void => {
-  console.log(message);
-};
+import { Command, Option, runExit } from "npm:clipanion";
+
+/**
+ * EchoCommand using clipanion
+ */
+runExit(
+  class EchoCommand extends Command {
+    static paths = [["echo"]];
+
+    text = Option.String({
+      required: true,
+    });
+
+    async execute() {
+      this.context.stdout.write(`${this.text}\n`);
+    }
+  }
+);
